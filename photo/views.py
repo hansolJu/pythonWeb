ef6d40e6f6a5aaea57dfb0537a4899bebd8fa492
DESKTOP-GTH902A\HansolJu <hansol1108@naver.com>
2018-04-11 14:23:44 +0900

    photo 기능 구현중...template 단만 완성하면됨

---
+++ photo/views.py
@@ -0,0 +1,16 @@
+from django.views.generic import ListView,DetailView
+from photo.models import Album,Photo
+
+# Create your views here.
+
+
+class AlbumLV(ListView):
+    model = Album
+
+
+class AlbumDV(DetailView):
+    model = Album
+
+
+class PhotoDV(DetailView):
+    model = Photo
